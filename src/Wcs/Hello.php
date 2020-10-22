f1db5e4aa2beee4a5fe4763e80ea3c89cd994f53
KadirWCS <71080068+KadirWCS@users.noreply.github.com>
2020-10-22 21:35:36 +0200

    Update Hello.php

--- src/Wcs/Hello.php
+++ src/Wcs/Hello.php
@@ -2,13 +2,21 @@
 
 namespace App\Wcs;
 
-class hello{
-
     private $talk = "Hello World";
 
 
     public function getTalk(): string
     {
         return $this->talk;
+    }
+}
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
     }
 }
